61aefc85bbbbc3f642e47798080bb0edd9ee6d99
taner <taner@DESKTOP-8FBSMG9>
2021-07-31 16:34:51 +0300

    Add project files.

---
+++ OOP1/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stok { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "Apple";
+            product.Id = 1;
+            product.Stok = 400;
+            product.Price = 10;
+
+
+
+            Product product1 = new Product();
+            product1.Name = "elma";
+            product1.Id = 4;
+            product1.Price = 550;
+            product1.Stok = 40;
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product);
+
+            productManager.Update(product1);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
